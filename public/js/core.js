aa43dd832064a2ed3f631dacebb4cbd138fca40c
JD <admin@jdplumbingsoflo.com>
2025-05-14 14:24:49 -0400

    updated valve test

--- public/js/core.js
+++ public/js/core.js
@@ -4,3 +4,13 @@ function generateDripCode() {
   const dailyCode = seed.split('').reduce((acc, c) => acc + c.charCodeAt(0), 0) % 100000;
   return `Drip-${dailyCode}`;
 }
+export function updateDrippyBackground(context) {
+  const bgMap = {
+    kitchen: "/drippy_assets/backgrounds/bg_kitchen.png",
+    bathroom: "/drippy_assets/backgrounds/bg_bathroom.png",
+    laundry: "/drippy_assets/backgrounds/bg_laundry.png",
+    garage: "/drippy_assets/backgrounds/bg_garage.png",
+    default: "/drippy_assets/backgrounds/bg_garage.png"
+  };
+  document.body.style.backgroundImage = `url('${bgMap[context] || bgMap.default}')`;
+}
